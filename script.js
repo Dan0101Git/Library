6b27db0e21912fd870e99185ca2b19cf5190a58a
Dan01011997 <f2016915@pilani.bits-pilani.ac.in>
2025-06-26 04:34:59 +0530

    gap inside card  along with read unrad button's funcitonality added

--- script.js
+++ script.js
@@ -1,171 +1,202 @@
-//define dom
+    //define dom
 
 
-const submit=document.querySelector("button[type='submit']");
-const plusButton=document.querySelector(".add-book button");
-const removeFormButton=document.querySelector(".remove-sidebar button");
-const mainClassList=document.querySelector("main").classList;
-const article=document.querySelector("article");
-//store image url's
-const imgArray=["3idiots","inception","interstellar2","udaan","whiplash"];
-//generate random url
-function generateRandomimgUrl(){
-    return `images/movies/${imgArray[rand(imgArray.length)]}.jpg`;
-}
-//random function fro rotate background images
-function rand(num){
-return Math.floor(Math.random()*num);
-}
-//change display to add form section(sidebar)
-plusButton.addEventListener("click",()=>{mainClassList.add("plus")
-    mainClassList.remove("minus");
-});
-removeFormButton.addEventListener("click",(e)=>{
+    const submit=document.querySelector("button[type='submit']");
+    const plusButton=document.querySelector(".add-book button");
+    const removeFormButton=document.querySelector(".remove-sidebar button");
+    const mainClassList=document.querySelector("main").classList;
+    const article=document.querySelector("article");
+    //store image url's
+    const imgArray=["3idiots","inception","interstellar2","udaan","whiplash"];
+    //generate random url
+    function generateRandomimgUrl(){
+        return `images/movies/${imgArray[rand(imgArray.length)]}.jpg`;
+    }
+    //random function fro rotate background images
+    function rand(num){
+    return Math.floor(Math.random()*num);
+    }
+    //change display to add form section(sidebar)
+    plusButton.addEventListener("click",()=>{mainClassList.add("plus")
+        mainClassList.remove("minus");
+    });
+    removeFormButton.addEventListener("click",(e)=>{
+        e.preventDefault();
+        mainClassList.remove("plus");
+        mainClassList.add("minus");
+    })
+
+    //submit button click
+    submit.addEventListener("click",(e)=>{
+        let title=document.querySelector("input#title").value;
+        let author=document.querySelector("input#author").value;
+        let pages=document.querySelector("input#pages").value;
+        let read=document.querySelector("input#read").value;
     e.preventDefault();
+
+
+    
+        addBookToLibrary(title,author,pages,read);
+    })
+
+
+
+
+
+
+
+
+
+    //store book into array
+    const myLibrary = [];
+
+    function Book(title,author,pages,read) {
+    this.title=title;
+    this.author=author;
+    this.pages=pages;
+    this.read=read;
+
+    }
+
+    function addBookToLibrary(title,author,pages,read) {
+    //add data to card array
+        const book=new Book(title,author,pages,read);
+        console.dir(book);
+    if(checkFormFilled(book)){myLibrary.push(book);
+        //Add unique id to each  book
+        book.id=crypto.randomUUID();
+        console.log(book.id);
+
+    createBookCard(book);}
+    else {
+        console.log("form invalid");
+        return;
+    }
+
+
+    }
+    function createBookCard(book){
+    
+    const newBookCard=document.createElement("div");
+    const cardImg=document.createElement("img");
+    cardImg.className="card-image";
+    newBookCard.className="card";
+    newBookCard.classList.add(book.id);
+    //add delete button to card
+    const deleteButton=document.createElement("button");
+    deleteButton.classList.add("delete-button");
+    deleteButton.textContent="---";
+    //add event listener to delete
+    deleteButton.addEventListener("click",deleteCard);
+    newBookCard.appendChild(deleteButton);
+    //add read button
+        const readButton=document.createElement("button");
+    readButton.classList.add("read-button");
+    readButton.textContent="Read";
+    //add event listener to delete
+    readButton.addEventListener("click",bookRead);
+    newBookCard.appendChild(readButton);
+
+
+        cardImg.setAttribute("src",generateRandomimgUrl());
+
+    newBookCard.appendChild(cardImg);
+
+    dataDisplay(book,newBookCard);
+    article.firstElementChild.insertBefore(newBookCard,article.firstElementChild.children[0]);
     mainClassList.remove("plus");
     mainClassList.add("minus");
-})
 
-//submit button click
-submit.addEventListener("click",(e)=>{
-    let title=document.querySelector("input#title").value;
-    let author=document.querySelector("input#author").value;
-    let pages=document.querySelector("input#pages").value;
-    let read=document.querySelector("input#read").value;
-e.preventDefault();
-
-
- 
-    addBookToLibrary(title,author,pages,read);
-})
-
-
-
-
-
-
-
-
-
-//store book into array
-const myLibrary = [];
-
-function Book(title,author,pages,read) {
-this.title=title;
-this.author=author;
-this.pages=pages;
-this.read=read;
-
-}
-
-function addBookToLibrary(title,author,pages,read) {
-//add data to card array
-    const book=new Book(title,author,pages,read);
-    console.dir(book);
-  if(checkFormFilled(book)){myLibrary.push(book);
-    //Add unique id to each  book
-    book.id=crypto.randomUUID();
-    console.log(book.id);
-
-createBookCard(book);}
-else {
-    console.log("form invalid");
     return;
-}
 
-
-}
-function createBookCard(book){
-   
-const newBookCard=document.createElement("div");
-const cardImg=document.createElement("img");
-cardImg.className="card-image";
-newBookCard.className="card";
-newBookCard.classList.add(book.id);
-//add delete button to card
-const deleteButton=document.createElement("button");
-deleteButton.classList.add("delete-button");
-deleteButton.textContent="---";
-//add event listener to delete
-deleteButton.addEventListener("click",deleteCard);
-newBookCard.appendChild(deleteButton);
-    cardImg.setAttribute("src",generateRandomimgUrl());
-
-newBookCard.appendChild(cardImg);
-
-dataDisplay(book,newBookCard);
-article.firstElementChild.insertBefore(newBookCard,article.firstElementChild.children[0]);
-   mainClassList.remove("plus");
-   mainClassList.add("minus");
-
-return;
-
-}
-function dataDisplay(book,newBookCard){
-    const bookKeys=Object.keys(book);
-    let bookEntryCount=0;
-    let flag=0;
-     bookKeys.forEach((key)=>{
-          bookEntryCount++;
-      if(flag===1){
-        return;
-      }
-          const cardNode=document.createElement("p");
-                  cardNode.textContent=book[key];
-        newBookCard.appendChild(cardNode);
-      cardNode.classList.add("card-font");
-              if(bookEntryCount<=2){
-if(bookEntryCount===1){
-    cardNode.classList.add("card-title");
-}
-else(cardNode.classList.add("card-author"));
-          }
-          else {
-            flag=1;
-            cardNode.textContent=`This book contains ${book["pages"]} pages and is ${book["read"]?"already read":"not read yet"}`;
-          }
-
-     });
-
-}
-
-//add delete functionality
-function deleteCard(e){
-   
-    console.log("i am here");
-    let index;
-    const filteredobject=  myLibrary.filter((book)=>{
-       if(book.id===e.target.parentNode.classList.value.slice(5,))
-        index=myLibrary.indexOf(book);
-        return book.id===e.target.parentNode.classList.value.slice(5,)});
-    myLibrary.splice(index,1);
-    e.target.parentNode.remove();
-   
-
-
-   
-} 
-
-function checkFormFilled(book){
-    let flag=1;
-// const bookKeys=Object.keys(book);
-const required=document.querySelectorAll("input[required]");
-Array.from(required).forEach((node)=>{
-    if(!book[node.getAttribute("id")])
-    {
-        node.classList.add("invalid");
-       flag=0;
-
-       console.log(Array.from(node.classList));
     }
-    else{
-          //remove invalid property
-        if(Array.from(node.classList).indexOf("invalid")+1){
-              console.log("invallid removed");
-            node.classList.remove("invalid");
-        
+    function dataDisplay(book,newBookCard){
+        const bookKeys=Object.keys(book);
+        let bookEntryCount=0;
+        let flag=0;
+        bookKeys.forEach((key)=>{
+            bookEntryCount++;
+        if(flag===1){
+            return;
         }
+            const cardNode=document.createElement("p");
+                    cardNode.textContent=book[key];
+            newBookCard.appendChild(cardNode);
+        cardNode.classList.add("card-font");
+                if(bookEntryCount<=2){
+    if(bookEntryCount===1){
+        cardNode.classList.add("card-title");
     }
+    else(cardNode.classList.add("card-author"));
+            }
+            else {
+                flag=1;
+                cardNode.textContent=`This book contains ${book["pages"]} pages `;
+            }
+
+        });
+
+    }
+function bookRead(e){
+let bookRead= myLibrary[detectCard(e)];
+let readButton=e.target;
+
+        if(bookRead.read==="on"){
+            readButton.classList.add("state-unread");
+            console.log(readButton);
+            bookRead.read="off";
+            readButton.textContent="UnRead"; 
+            return;
+
+        }
+        if(bookRead.read==="off"){
+                readButton.classList.remove("state-unread");
+            bookRead.read="on";
+            readButton.textContent="Read";
+        }
+    
+}
+    //add delete functionality
+    function deleteCard(e){
+    
+        console.log("i am here");
+
+        myLibrary.splice(detectCard(e),1);
+        e.target.parentNode.remove();
+    
+
+
+    
+    } 
+    function detectCard(e){
+              let index;
+        const filteredobject=  myLibrary.filter((book)=>{
+        if(book.id===e.target.parentNode.classList.value.slice(5,))
+            index=myLibrary.indexOf(book);
+            return book.id===e.target.parentNode.classList.value.slice(5,)});
+            return index;
+    }
+
+    function checkFormFilled(book){
+        let flag=1;
+    // const bookKeys=Object.keys(book);
+    const required=document.querySelectorAll("input[required]");
+    Array.from(required).forEach((node)=>{
+        if(!book[node.getAttribute("id")])
+        {
+            node.classList.add("invalid");
+        flag=0;
+
+        console.log(Array.from(node.classList));
+        }
+            else{
+            //remove invalid property
+            if(Array.from(node.classList).indexOf("invalid")+1){
+                console.log("invallid removed");
+                node.classList.remove("invalid");
+            
+            }
+}
 })
 if(flag)
 return true;
